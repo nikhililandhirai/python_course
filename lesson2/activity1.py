0d6b1534c3ea32f019c7d14b934c6073d0be1cb7
nikhililandhirai <nikilan486@gmail.com>
2025-11-01 22:19:26 +0530

    done

---
+++ lesson2/activity1.py
@@ -0,0 +1,12 @@
+name="nikhil"
+age=21
+student=True
+weight=38.5
+print("name: ",name)
+print("datatype of name is: ",type(name))
+print("age: ",age)
+print("datatype of age is: ",type(age))
+print("student: ",student)
+print("datatype of student is: ",type(student))
+print("weight:",weight)
+print("datatype of weight is: ",type)
---
+++ lesson2/activity2.py
@@ -0,0 +1,6 @@
+name="codingal"
+print("name: ",name)
+print("length of this string: ",len(name))
+print("first letter of the string: ",name[0])
+print("last letter of the string: ",name[-1])
+print("string sliced: ",name[0:3])
